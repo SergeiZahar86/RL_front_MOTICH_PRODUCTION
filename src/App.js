fb049a0b70059d3de5adcf6b683a1461d7c151aa
Сергей Захаров <sergei86zahar@mail.ru>
2021-03-09 06:33:29 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Switch} from "react-router-dom";
+import General from "./routes/General/General";
+import {Route} from "react-router-dom";
+import Courses from "./routes/Courses/Courses";
+import News from "./routes/News/News";
+import Shop from "./routes/Shop/Shop";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route path="/" exact component={General}/>
+            <Route path="/courses" component={Courses}/>
+            <Route path="/news" component={News}/>
+            <Route path="/shop" component={Shop}/>
+        </Switch>
+    );
 }
 
 export default App;
